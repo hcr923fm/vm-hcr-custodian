246a9e5bfbdbe00638cf88bd2cdfa0634198b20f
calmcl1 <calmcl1@aol.com>
2019-02-02 01:14:24 +0300

    Sort out permissions, use the right host

--- init.sh
+++ init.sh
@@ -6,7 +6,9 @@ git clone http://github.com/hcr923fm/custodian
 cd custodian
 
 sudo npm install -g @angular/cli
+sudo chmod 777 -R ./
 npm install
 echo "AUDIO_FILE_DIRECTORY='/var/LoggedAudio'" > .env
+echo "CUSTODIAN_HOST='192.168.0.130'" >> .env
 
 ng build && npm start
