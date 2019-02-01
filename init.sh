52225ee16ff238e17c5ffc4b1d91e506b033bbe9
calmcl1 <calmcl1@aol.com>
2019-02-02 00:10:49 +0300

    Added basic build script

--- init.sh
+++ init.sh
@@ -1,3 +1,11 @@
 curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
 sudo apt-get update
-sudo apt-get install -y nodejs npm build-essential
+sudo apt-get install -y nodejs npm build-essential git
+
+git clone https://github.com/hc923fm/custodian
+cd custodian
+
+npm install
+echo "/var/LoggedAudio" > .env
+
+npm build && npm start
